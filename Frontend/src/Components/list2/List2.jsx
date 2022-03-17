cac307eb47dfa387fee7f673e976037b59a9c556
Tadeo <teoleon2013@gmail.com>
2022-03-17 15:58:31 -0300

    se agregaron styles a los next y prev button

--- Frontend/src/Components/list2/List2.jsx
+++ Frontend/src/Components/list2/List2.jsx
@@ -2,7 +2,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import SwiperCore, { Navigation, A11y } from "swiper";
 
 // Import Swiper styles
 import "./List2.scss";
@@ -13,7 +13,7 @@ import {
 } from "@mui/icons-material";
 
 // install Swiper modules
-SwiperCore.use([Navigation, Pagination, A11y]);
+SwiperCore.use([Navigation, A11y]);
 
 export default () => {
   return (
@@ -23,9 +23,9 @@ export default () => {
       </button>
       <Swiper
         className="swiper"
-        modules={[Navigation, Pagination, A11y]}
-        spaceBetween={15}
-        slidesPerView={4.4}
+        modules={[Navigation, A11y]}
+        spaceBetween={10}
+        slidesPerView={5.5}
         navigation={{
           nextEl: ".custom_next",
           prevEl: ".custom_prev",
@@ -34,18 +34,18 @@ export default () => {
         // onSwiper={(swiper) => console.log(swiper)}
         // onSlideChange={() => console.log("slide change")}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-        <SwiperSlide>Slide 10</SwiperSlide>
-        <SwiperSlide>Slide 11</SwiperSlide>
-        <SwiperSlide>Slide 12</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 1</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 2</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 3</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 4</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 5</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 6</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 7</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 8</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 9</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 10</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 11</SwiperSlide>
+        <SwiperSlide class="swiper-slide">Slide 12</SwiperSlide>
       </Swiper>
       <button class="custom_next">
         <ArrowForwardIosOutlined />
