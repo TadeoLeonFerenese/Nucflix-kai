f44576024f13a9bf968e43327632413b3f259aa7
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2022-03-17 22:59:01 -0300

    List2 esta ready falta crear los list items2

--- Frontend/src/Components/list2/List2.jsx
+++ Frontend/src/Components/list2/List2.jsx
@@ -17,39 +17,84 @@ SwiperCore.use([Navigation, A11y]);
 
 export default () => {
   return (
-    <div className="container">
-      <button class="custom_prev">
-        <ArrowBackIosOutlined />
-      </button>
-      <Swiper
-        className="swiper"
-        modules={[Navigation, A11y]}
-        spaceBetween={10}
-        slidesPerView={5.5}
-        navigation={{
-          nextEl: ".custom_next",
-          prevEl: ".custom_prev",
-        }}
-        pagination={{ clickable: true }}
-        // onSwiper={(swiper) => console.log(swiper)}
-        // onSlideChange={() => console.log("slide change")}
-      >
-        <SwiperSlide class="swiper-slide">Slide 1</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 2</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 3</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 4</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 5</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 6</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 7</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 8</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 9</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 10</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 11</SwiperSlide>
-        <SwiperSlide class="swiper-slide">Slide 12</SwiperSlide>
-      </Swiper>
-      <button class="custom_next">
-        <ArrowForwardIosOutlined />
-      </button>
+    <div class="Swiper-container">
+      <div class="swiper-wrapper">
+        <button class="custom_prev">
+          <ArrowBackIosOutlined />
+        </button>
+        <Swiper
+          className="swiper"
+          modules={[Navigation, A11y]}
+          spaceBetween={10}
+          slidesPerView={5.5}
+          slide
+          navigation={{
+            nextEl: ".custom_next",
+            prevEl: ".custom_prev",
+          }}
+          pagination={{ clickable: true }}
+          // onSwiper={(swiper) => console.log(swiper)}
+          // onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+        </Swiper>
+        <button class="custom_next">
+          <ArrowForwardIosOutlined />
+        </button>
+      </div>
     </div>
   );
 };
