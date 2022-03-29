8973cbd7ec2c75656bb012215f1ee491b353b90a
Tadeo Leon Ferenese <teoleon2013@gmail.com>
2022-03-29 15:48:34 -0300

    Faltan pulird etalles en styles del swiper

--- Frontend/src/Components/list2/List2.jsx
+++ Frontend/src/Components/list2/List2.jsx
@@ -20,84 +20,84 @@ SwiperCore.use([Navigation, A11y]);
 
 export default () => {
   return (
-    <div class="Swiper-container">
-      <div class="swiper-wrapper">
-        <button class="custom_prev">
-          <ArrowBackIosOutlined />
-        </button>
-        <Swiper
-          className="swiper"
-          modules={[Navigation, A11y]}
-          spaceBetween={10}
-          slidesPerView={5.2}
-          slide
-          navigation={{
-            nextEl: ".custom_next",
-            prevEl: ".custom_prev",
-          }}
-          pagination={{ clickable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img
-              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-              alt="img"
-            />
-          </SwiperSlide>
-        </Swiper>
-        <button class="custom_next">
-          <ArrowForwardIosOutlined />
-        </button>
-      </div>
+    <div class="swiper-container">
+      {/* <div class="wrapper"> */}
+      <button class="custom_prev">
+        <ArrowBackIosOutlined />
+      </button>
+      <Swiper
+        className="swiper"
+        modules={[Navigation, A11y]}
+        spaceBetween={10}
+        slidesPerView={5.2}
+        slide
+        navigation={{
+          nextEl: ".custom_next",
+          prevEl: ".custom_prev",
+        }}
+        pagination={{ clickable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+            alt="img"
+          />
+        </SwiperSlide>
+      </Swiper>
+      <button class="custom_next">
+        <ArrowForwardIosOutlined />
+      </button>
     </div>
+    // </div>
   );
 };
