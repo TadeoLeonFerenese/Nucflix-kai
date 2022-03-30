915da4b3134e8e008f92f8e270a8a72cb176d5bf
Tadeo <teoleon2013@gmail.com>
2022-03-30 15:26:18 -0300

    quedo re loco el swiper

--- Frontend/src/Components/list2/List2.jsx
+++ Frontend/src/Components/list2/List2.jsx
@@ -1,5 +1,4 @@
 // Import Swiper React components
-import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../listitem2/listitem2.scss";
 
@@ -21,83 +20,83 @@ SwiperCore.use([Navigation, A11y]);
 export default () => {
   return (
     <div class="swiper-container">
-      {/* <div class="wrapper"> */}
-      <button class="custom_prev">
-        <ArrowBackIosOutlined />
-      </button>
-      <Swiper
-        className="swiper"
-        modules={[Navigation, A11y]}
-        spaceBetween={10}
-        slidesPerView={5.2}
-        slide
-        navigation={{
-          nextEl: ".custom_next",
-          prevEl: ".custom_prev",
-        }}
-        pagination={{ clickable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img
-            src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
-            alt="img"
-          />
-        </SwiperSlide>
-      </Swiper>
-      <button class="custom_next">
-        <ArrowForwardIosOutlined />
-      </button>
+      <div class="swiper-wrapper">
+        <button class="custom_prev">
+          <ArrowBackIosOutlined />
+        </button>
+        <Swiper
+          className="swiper"
+          modules={[Navigation, A11y]}
+          spaceBetween={10}
+          slidesPerView={5.2}
+          slide
+          navigation={{
+            nextEl: ".custom_next",
+            prevEl: ".custom_prev",
+          }}
+          pagination={{ clickable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src="https://www.quever.news/u/fotografias/m/2021/2/12/f1280x720-6654_138329_5050.jpg"
+              alt="img"
+            />
+          </SwiperSlide>
+        </Swiper>
+        <button class="custom_next">
+          <ArrowForwardIosOutlined />
+        </button>
+      </div>
     </div>
-    // </div>
   );
 };
--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import "./home.scss";
 import Navbar from "../../Components/navbar/Navbar";
 import Featured from "../../Components/featured/Featured";
-import List from "../../Components/list/List";
+// import List from "../../Components/list/List";
 import Footer from "../../Components/footer/Footer";
 import List2 from "../../Components/list2/List2";
 
@@ -10,10 +10,10 @@ const home = () => {
     <div className="home">
       <Navbar />
       <Featured />
-      <List page={1} />
-      <List page={2} />
-      <List page={3} />
-      <List page={4} />
+      <List2 />
+      <List2 />
+      <List2 />
+      <List2 />
       <List2 />
       <Footer />
     </div>
